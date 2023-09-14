2bf80ffc526405a2d65b96d2c7396c072e25e4ac
Supraja Ramaraju <test.email@example.com>
2023-09-15 00:47:24 +0100

    Added TaskItems and TaskService classes to the repository

---
+++ BarclaysToDoApp/Models/TaskItems.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BarclaysToDoApp.Models
+{
+    public class TaskItems
+    {
+        
+        public int TaskId { get; set; }
+
+        [Required(ErrorMessage = "Task Name field is required.")]
+        public string TaskName { get; set; }
+
+        [Required(ErrorMessage ="Priority field is required.")]
+        [Range(1, 5, ErrorMessage = "Please select the Priority between 1 and 5")]
+        public int Priority { get; set; }
+
+        [Required(ErrorMessage = "Status field is required.")]
+        public string Status { get; set; }
+    }
+}
---
+++ BarclaysToDoApp/Services/TaskServices.cs
@@ -0,0 +1,48 @@
+﻿using BarclaysToDoApp.Models;
+
+namespace BarclaysToDoApp.Services
+{
+    public class TaskServices
+    {
+        // Adding in-memory collection to store the tasks
+        private List<TaskItems> tasks;
+
+        
+        public TaskServices() 
+        {
+            // To initialize the tasks list here
+            tasks = new List<TaskItems>();
+        }
+
+        public void AddTask(TaskItems task) 
+        {
+            tasks.Add(task);
+        }
+
+        // Method to Edit a task by it's ID
+        public void EditTask(int taskID, TaskItems updatedTask) 
+        {
+            TaskItems currentTask = tasks.FirstOrDefault(t => t.TaskId == taskID);
+            if (currentTask != null) 
+            {
+                currentTask.TaskName = updatedTask.TaskName;
+                currentTask.Priority = updatedTask.Priority;
+                currentTask.Status = updatedTask.Status;
+            }
+        }
+
+        public List<TaskItems> GetTasks() 
+        {
+            return tasks;
+        }
+
+        public void DeleteTask(int taskID) 
+        {
+            TaskItems taskToRemove = tasks.FirstOrDefault(t => t.TaskId == taskID);
+            if (taskToRemove != null) 
+            {
+                tasks.Remove(taskToRemove);
+            }
+        }
+    }
+}
