e714ac4fe97aefed0d97707fcb23180a495feccd
Supraja Ramaraju <test.email@example.com>
2023-09-18 19:20:38 +0100

    Changes to the CreateTask.cshtml file

--- BarclaysToDoApp/Models/TaskItems.cs
+++ BarclaysToDoApp/Models/TaskItems.cs
@@ -8,13 +8,13 @@ namespace BarclaysToDoApp.Models
         public int TaskId { get; set; }
 
         [Required(ErrorMessage = "Task Name field is required.")]
-        public string TaskName { get; set; }
+        public string ?TaskName { get; set; }
 
         [Required(ErrorMessage ="Priority field is required.")]
         [Range(1, 5, ErrorMessage = "Please select the Priority between 1 and 5")]
         public int Priority { get; set; }
 
         [Required(ErrorMessage = "Status field is required.")]
-        public string Status { get; set; }
+        public string ?Status { get; set; }
     }
 }
